3ee49f358272c7681a780cdd3149a36c9b8c4640
imoempire <mrimoempire@gmail.com>
2021-06-16 17:21:15 +0000

    add Plan component

---
+++ src/Component/Plans.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import
+
+const Plans = () => {
+    return (
+        <div className="container plans">
+        <h2 className="text-center">      PLANS        </h2>
+      <div className="row">
+        <div className="col-md-3">
+          <div className="thumbnail">
+          <img src="images/luke-chesser-wnShDP37vB4-unsplash.jpg" alt="office" />
+          
+        <h4>VIRTUAL OFFICE</h4>
+        <p>Our virtual offices allow your business to get our location, 
+          phone services and also you become a member of our community with benefit such as to our meeting room</p>
+      </div>
+    </div>
+        <div className="col-md-3">
+          <div className="thumbnail">
+            <img src="images/nastuh-abootalebi-ZtC4_rPCRXA-unsplash.jpg" alt="office" />
+          
+          <h4> CUSTOMIZED OFFICE</h4>
+          <p>Our virtual offices allow your business to get our location, 
+            phone services and also you become a member of our community with benefit such as to our meeting room</p>
+        </div>
+          </div>
+
+        <div className="col-md-3">
+          <div className="thumbnail">
+          <img src="images/nastuh-abootalebi-eHD8Y1Znfpk-unsplash.jpg" alt="office" />
+        
+        <h4>SERVICED OFFICE</h4>
+        <p>
+          Our virtual offices allow your business to get our location, 
+          phone services and also you become a member of our community with benefit such as to our meeting room
+        </p>
+      </div>
+    </div>
+
+        <div className="col-md-3">
+          <div className="thumbnail">
+            <img src="images/benjamin-child-0sT9YhNgSEs-unsplash.jpg" alt="office" />
+            <div className="caption">
+              <h4>MEETING ROOM</h4>
+              <p> Our virtual offices allow your business to get our location, 
+                phone services and also you become a member of our community with benefit such as to our meeting room</p>
+            </div>
+          </div>
+      </div>
+
+      </div>
+    </div>
+
+    );
+}
+
+export default Plans;
