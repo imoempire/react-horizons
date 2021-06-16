a6fbbc5b0822d08f0c5ae4a02c5c2d5563a284f6
imoempire <mrimoempire@gmail.com>
2021-06-16 17:27:16 +0000

    add Plans Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import Carousel from "./Component/Carousel";
 import Navbar from "./Component/Navbar";
+import Plans from "./Component/Plans";
 
 function App() {
   return (
     <>
     <Navbar />
+    <Carousel />
+    <Plans />
     </>
   );
 }
